fd6680faf0911d23070ac1e7a3c0a486736e0b72
sr0116 <kysy0116@gmail.com>
2025-11-26 18:54:37 +0900

    graphql 연습용 employee

--- src/main/java/com/imchbo/exgraphql/repository/EmployeeRepository.java
+++ src/main/java/com/imchbo/exgraphql/repository/EmployeeRepository.java
@@ -4,4 +4,5 @@ import com.imchbo.exgraphql.domain.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+//
 }
